90f86addb7bc3bd5b68994bd7a4d399a11f10a03
JIMMY <JIMMY@DESKTOP-LOMPCIS>
2025-09-11 10:34:51 -0600

    comienzo de creacion de base de datos

---
+++ WindowsFormsApp1/CONEXION.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;// esta libreria me permite usar comando para trabajar con base de datos
+using System.Data.SqlClient;// esta libreria me permite trabajar  con  SQL server
+namespace WindowsFormsApp1
+{
+    internal class CONEXION
+    {
+     //definir los miembros de la clase ,atributos y metodos,
+     SqlConnection objConexion = new SqlConnection(); //conectarme a la base de datos 
+        SqlCommand objComando = new SqlCommand();// ejecutar sql en bd lectura,actualizacion ,eliminacion,insercion
+        SqlDataAdapter objAdaptador = new SqlDataAdapter(); //un puente entre la bd y la aplicacioon  
+}
+}
