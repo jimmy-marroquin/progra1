ce648a3e2b9b2e0cc5492a6411d79a23a049dcc8
Ulloa <Ulloa@LAPTOP-C4A4PTCQ>
2025-09-10 18:07:10 -0700

    Se creo una calculadora de sueldos

--- primer proyecto/primer proyecto/Program.cs
+++ primer proyecto/primer proyecto/Program.cs
@@ -16,7 +16,7 @@ namespace primer_proyecto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form4());
+            Application.Run(new Form5());
         }
     }
 }
